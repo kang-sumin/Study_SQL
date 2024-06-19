0bc1c20f3c076f3572e68a4d71b0870d78560a98
kang-sumin <rkdtnals1117@naver.com>
2024-06-19 17:33:52 +0900

    Create SQL 데이터 조회하기(SELECT,FROM).sql

---
+++ Sparta_DBeaver_SQL/SQL 데이터 조회하기(SELECT,FROM).sql
@@ -0,0 +1,11 @@
+/*
+ * SELECT : 데이터를 가져오는 기본 명령어로, 데이터를 조회하는 모든 Query에 사용됨
+ * FROM : 데이터를 가져올 테이블을 특정해주는 문법
+ * * : 모든 컬럼을 가져와준다는 의미
+ * */
+
+SELECT * FROM food_orders fo;
+
+SELECT * FROM payments p ;
+
+SELECT * FROM customers c ;
